9394b0247ae8b5b6daff4e047a79bf569734af4a
Nikant Yadav <nikantyadav6803@gmail.com>
2024-05-06 20:26:20 +0530

    Downloading mp4, and converting to mp3

--- youtubemp3.py
+++ youtubemp3.py
@@ -1,7 +1,9 @@
 from pytube import YouTube
 from pytube.exceptions import VideoUnavailable
+from moviepy.editor import *
 
-yt = YouTube('https://youtu.be/Lh-YS0IczmM?si=Awg4kLoa6m0we1hO')
+
+yt = YouTube('https://youtu.be/g4xs_5rZdos?si=OtCknKQbwazl4nkA')
 streams = yt.streams
 audiostreams = yt.streams.filter(only_audio=True)
 teststream = yt.streams.get_by_itag('140')
@@ -17,4 +19,12 @@ print(best_quality)
 best_quality_index = int_quality.index(best_quality)
 
 downloadable_stream = audiostreams[best_quality_index]
-downloadable_stream.download(output_path = 'C:\\Users\\Nikant Yadav\\Desktop\\Programming\\Anti Spotify App\\downloaded')
+downloadable_stream.download(output_path = 'C:\\Users\\Nikant Yadav\\Desktop\\Programming\\Anti Spotify App\\downloaded\\',filename = 'video.mp4', max_retries = 5)
+
+
+print('Your video file has been downloaded')
+
+inputfile = 'C:\\Users\\Nikant Yadav\\Desktop\\Programming\\Anti Spotify App\\downloaded\\video.mp4'
+outputfile = 'C:\\Users\\Nikant Yadav\\Desktop\\Programming\\Anti Spotify App\\mp3\\audio.mp3'
+
+AudioFileClip(inputfile).write_audiofile(outputfile, bitrate='3000k')
