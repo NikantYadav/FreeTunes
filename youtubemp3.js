bd93d59b69bc8add2af22d1b266a67dbfd3dc89e
Nikant Yadav <nikantyadav6803@gmail.com>
2024-05-10 11:30:20 +0530

    !

--- youtubemp3.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-
-const videoURL = prompt("Input the video URL");
-const outputFilePath = 'downloaded/video.mp4';
-
-const audio = ytdl(videoURL, { quality: 'highestaudio' })
